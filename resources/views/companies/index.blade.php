6a4a3717dc84b953c70821371f265ab852b8d1b0
Arvind <arvind@taldartech.in>
2023-05-17 16:21:24 +0530

    list all companies view & route implementation

---
+++ resources/views/companies/index.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>CRUD App</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" >
+</head>
+<body>
+    <div class="container mt-2">
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>S.No</th>
+                    <th>Company Name</th>
+                    <th>Company Email</th>
+                    <th>Company Address</th>
+                    <th width="280px">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($companies as $company)
+                    <tr>
+                        <td>{{ $company->id }}</td>
+                        <td>{{ $company->name }}</td>
+                        <td>{{ $company->email }}</td>
+                        <td>{{ $company->address }}</td>
+                        <td>
+                            <form action="{{ route('companies.destroy',$company->id) }}" method="Post">
+                                <a class="btn btn-primary" href="{{ route('companies.edit',$company->id) }}">Edit</a>
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
